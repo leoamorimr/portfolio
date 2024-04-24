b93a074845c7d8ef4f7b2006a6abaf53c107ff8e
Leonardo A. Ramos <leonardo.a.ramos@accenture.com>
2024-04-24 16:43:53 +0100

    css fixes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,19 +20,18 @@ import Vue from '../assets/vue.png';
 
 const Skills = () => {
     return (
-        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div name='skills' className='w-full h-full bg-[#0a192f] text-gray-300'>
             {/* Container */}
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-                <div className='mb-4'>
+                <div>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
                     <p className='py-4'>// These are the technologies I've worked with</p>
                 </div>
 
-
-                {/* Front-End skills */}
-                <div>
+                <div className='w-full py-8'>
                     <p className='text-2xl font-bold inline border-b-2 border-pink-600 '>Front-End</p>
                 </div>
+
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                         <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
@@ -51,7 +50,7 @@ const Skills = () => {
                         <p className='my-4'>JAVASCRIPT</p>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                        <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
                         <p className='my-4'>REACT</p>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
@@ -60,8 +59,7 @@ const Skills = () => {
                     </div>
                 </div>
 
-                {/* Back-End skills */}
-                <div>
+                <div className='w-full py-8'>
                     <p className='text-2xl font-bold inline border-b-2 border-pink-600 '>Back-End</p>
                 </div>
 
@@ -71,82 +69,73 @@ const Skills = () => {
                         <img className='w-20 mx-auto' src={Java} alt="Java icon" />
                         <p className='my-4'>JAVA</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                         <img className='w-20 mx-auto' src={Node} alt="Node icon" />
                         <p className='my-4'>NODE JS</p>
                     </div>
                 </div>
 
-                {/* Databases skills */}
-                <div>
+                <div className='w-full py-8'>
                     <p className='text-2xl font-bold inline border-b-2 border-pink-600 '>Databases</p>
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Mysql} alt="MySQL icon" />
+                        <img className='w-20 mx-auto' src={Mysql} alt='Mysql icon' />
                         <p className='my-4'>MYSQL</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Oracle} alt="Oracle icon" />
+                        <img className='w-20 mx-auto' src={Oracle} alt='Azure icon' />
                         <p className='my-4'>ORACLE</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Sqlserver} alt="SQLServer icon" />
+                        <img className='w-20 mx-auto' src={Sqlserver} alt='SQLServer icon' />
                         <p className='my-4'>SQL SERVER</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Postgre} alt="Postgre icon" />
+                        <img className='w-20 mx-auto' src={Postgre} alt='Postgre icon' />
                         <p className='my-4'>POSTGRE</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Mongo} alt="Mongo icon" />
-                        <p className='my-4'>MONGO DB</p>
+                        <img className='w-20 mx-auto' src={Mongo} alt='MongoDB icon' />
+                        <p className='my-4'>MONGODB</p>
                     </div>
                 </div>
 
-                {/* Cloud skills */}
-                <div>
+
+
+                <div className='w-full py-8'>
                     <p className='text-2xl font-bold inline border-b-2 border-pink-600 '>Cloud</p>
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Aws} alt="Aws icon" />
+                        <img className='w-20 mx-auto' src={Aws} alt='AWS icon' />
                         <p className='my-4'>AWS</p>
                     </div>
-
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Azure} alt="Azure icon" />
+                        <img className='w-20 mx-auto' src={Azure} alt='Azure icon' />
                         <p className='my-4'>AZURE</p>
                     </div>
                 </div>
 
-                {/* DevOps skills */}
                 <div>
                     <p className='text-2xl font-bold inline border-b-2 border-pink-600 '>DevOps</p>
                 </div>
-
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
 
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Docker} alt="Docker icon" />
+                        <img className='w-20 mx-auto' src={Docker} alt='Docker icon' />
                         <p className='my-4'>DOCKER</p>
                     </div>
 
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Kubernetes} alt="Kubernetes icon" />
+                        <img className='w-20 mx-auto' src={Kubernetes} alt='Kubernetes icon' />
                         <p className='my-4'>KUBERNETES</p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
